0decc91d503b4438f8d937f29c4dfa65bd37cd62
Pedro Revilla <peterevilla@gmail.com>
2020-03-14 15:36:35 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  state = {
+    user: [],
+    followers:[]
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/peterevilla")
+      .then(res => res.json())
+      .then(users => {
+        console.log(users);
+        this.setState({ user: users });
+        console.log(this.state.user);
+        return fetch('https://api.github.com/users/peterevilla/followers')
+      })
+      .then(res => res.json())
+      .then(follow => {
+        console.log('followers:', follow)
+        this.setState({ followers: follow });
+      })
+      .catch(err => console.error(err));
+  }
+
+  // componentDidMount() {
+  //   fetch("https://api.github.com/users/peterevilla/followers")
+  //     .then(res => res.json())
+  //     .then(follows => {
+  //       console.log(follows);
+  //       this.setState({ followers: follows });
+  //       console.log(this.state.followers);
+  //     })
+  //     .catch(err => console.error(err));
+  // }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Get Github User Card</h1>
+        <Card>
+        <Image src={this.state.user.avatar_url} />
+        <Card.Content>
+        <Card.Header>{this.state.user.name}</Card.Header>
+        <Card.Meta><a href={this.state.user.html_url}><Icon name='github'/> {this.state.user.login}</a></Card.Meta>
+        {this.state.followers.map(ele => (
+          <ul>
+            <li>{ele.login}</li>
+          </ul>
+        ))}
+        </Card.Content>
+        </Card>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
