5540e9baf744893415622ec1b3320d9017689a1a
IT2 <it2@AWT.local>
2020-03-17 13:32:17 -0500

    wip

--- src/components/FormSearch.js
+++ src/components/FormSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 
 
@@ -8,32 +8,20 @@ import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 
 class FormSearch extends React.Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                query: ''
-            }
-        }
+    constructor(props) {
+        super(props)
 
     
+    }
 
 
-
-
-
-
-    handleChanges = e => {
-        this.setState({ ...this.state, query: e.target.value });
-        console.log(this.state.query)
-
-    };
 
     handleSubmit = e => {
         e.preventDefault();
-        this.props.handleQuery(this.state.query)
-        this.props.onSearch()
-        console.log('query', this.state.query)
-        this.setState({ query: ''})
+        console.log(e.target.value)
+        this.props.setSearchTerm(e.target.value)
+        // this.props.onSearch()
+
 
     }
 
@@ -44,11 +32,11 @@ class FormSearch extends React.Component {
         return (
             <div>
                 <Form className='form' onSubmit={this.handleSubmit}>
-                <Form.Field>
-                    <label>
-                        <input type='text' name='query' placeholder='search github username' value={this.state.query} onChange={this.handleChanges}></input>
-                    </label>
-                </Form.Field>
+                    <Form.Field>
+                        <label>
+                            <input type='text' placeholder='search github username'  ></input>
+                        </label>
+                    </Form.Field>
                     <label>
                         <Button type='submit'>search</Button>
                     </label>
